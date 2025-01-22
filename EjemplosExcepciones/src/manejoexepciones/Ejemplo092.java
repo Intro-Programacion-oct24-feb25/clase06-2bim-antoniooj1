76c4f637db73db2514a260ff5736e8d1c75ebab6
Antonio Ojeda <ajojeda6@utpl.edu.ec>
2025-01-22 12:34:12 -0500

    Manejo de excepciones Ejemplo06 ilegal convertion

---
+++ EjemplosExcepciones/src/manejoexepciones/Ejemplo092.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manejoexepciones;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo092 {
+
+    public static void main(String[] args) {
+        boolean bandera = true;
+        Scanner entrada = new Scanner(System.in);
+
+        while (bandera) {
+            try {
+                System.out.println("Ingrese el nombre de un pais: ");
+                String pais = entrada.nextLine();
+                char vocal = pais.charAt(0);
+
+                if (vocal == 'a' || vocal == 'e' || vocal == 'i' || vocal == 'o'
+                        || vocal == 'u') {
+                    bandera = true;
+                }else{
+                    throw new Exception("No inicia con una vocal");
+                }
+
+            } catch (Exception e) {
+                System.out.printf("Ocurrió una excepción %s\n", e);
+                bandera = false;
+            }
+        }
+    }
+}
